0bdf7a5d14c7af79251f1d5b9652020be34c0a87
Rundll86 <2028673870@qq.com>
2025-07-28 11:49:20 +0800

    Refactor code structure for improved readability and maintainability

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vuepress/config";
+export default defineConfig({
+    themeConfig: {
+        sidebar: "auto",
+        sidebarDepth: 5
+    },
+    dest: "dist",
+    locales: {
+        "/": {
+            lang: "zh-CN",
+            title: "FS-Context",
+            description: "一个易用的 ScratchExtension TypeScript 上下文"
+        }
+    },
+    markdown: {
+        lineNumbers: true
+    }
+})
